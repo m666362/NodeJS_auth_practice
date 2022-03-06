5833fc4e166f4245d7b39b483123c767fc248296
Md. Rayhan <55645384+m666362@users.noreply.github.com>
2022-03-06 12:45:39 +0600

    data

---
+++ data.js
@@ -0,0 +1,18 @@
+const ROLE = {
+    ADMIN: "admin",
+    BASIC: "basic"
+}
+
+module.exports = {
+    ROLE,
+    users: [
+        {id: 1, name: "Rayhan", role: ROLE.ADMIN},
+        {id: 1, name: "Jihan", role: ROLE.BASIC},
+        {id: 1, name: "Marjana", role: ROLE.BASIC},
+    ],
+    projects: [
+        {id: 1, name: "Rayhan's project", userId: 1},
+        {id: 1, name: "Jihan's project", userId: 2},
+        {id: 1, name: "Marjana's project", userId: 3},
+    ]
+}
